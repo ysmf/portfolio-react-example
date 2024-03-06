5abde99a9eda96cf5c3212eccc66603eb12d7978
ysmf <150317829+ysmf@users.noreply.github.com>
2024-03-06 15:59:42 +0000

    4

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React , { useState } from "react";
 import styles from "./Navbar.module.css";
+import { FaBars } from "react-icons/fa6";
 
 function Navbar() {
+
+  const [isToggle, setToggle] = useState(false);
+
+  function handleToggle() {
+    setToggle(!isToggle);
+  }
+
+
   return (
     <nav>
       <div className={styles.container}>
         <div className={styles.nav_con}>
           <div className={styles.logo}>
-            <a href="#">TT Port</a>
+            <a href="#"> TT Port</a>
           </div>
 
           <ul>
@@ -20,6 +29,23 @@ function Navbar() {
             <a href="#">Hire Me</a>
           </div>
         </div>
+
+        {/* Mobile Menu */}
+        < FaBars className={styles.bars} onClick={handleToggle} />
+        {isToggle ? (
+          <>
+            <ul className={styles.mobile_menu}>
+              <li><a href="#">Skills</a></li>
+              <li><a href="#">Portfolio</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+
+            <div className={styles.mobile_button}>
+              <a href="#">Hire Me</a>
+            </div>
+          </>
+        ) : null
+        }
       </div>
     </nav>
   );
